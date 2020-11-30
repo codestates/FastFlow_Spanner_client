9da6e8c802e255cb90b2ebc46c7c3d9d653b879c
ryhyn123 <ryhyn123@gmail.com>
2020-11-30 16:42:43 +0900

    [Client]UPDATE:nav/detail update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { Switch, Route, withRouter } from "react-router-dom";
 import SignUp from "./components/SignUp"
+import Nav from "./components/Nav"
 
 
 const App = () => {
   
   return (
     <div>
+      <Nav />
       <Switch>
       <Route exact path='/signup' render={() => <SignUp />} />
       </Switch>
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Nav(props) {
-    return (
-        <header className="navs">
-            <div>
-                <Link to='/SignIn'>Log in</Link>
-                <Link to='/SignUp'>Sign up</Link>
-            </div>
-            <div>
-                <Link to='/Pages/MainPage'>logo</Link>
-            </div>
-        </header>
-    )
+export default function Nav() {
+  return (
+    <header className="navs">
+			<div className="nav__main">
+        <div className="nav__hamburgerBtn">
+          <Link to='/SignIn' className="nav__logInBtn">Log in</Link>
+          <Link to='/SignUp' className="nav__signUpBtn">Sign up</Link>
+        </div>
+			  <div className="nav__year">여기에 년도 입력</div>
+        <div>
+          <Link to='/Pages/MainPage' className="nav__logoBtn">logo</Link>
+        </div>
+			</div>
+			<div className="nav__scrollTracer">여기에 원숭이 입력</div>
+    </header>
+  )
 }
