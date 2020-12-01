f63bb3e61195dde2da2b7f5f208b16f6ed240546
ryhyn123 <ryhyn123@gmail.com>
2020-12-01 17:52:39 +0900

    [Client]UPDATE:nav/elements base position

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -8,13 +8,16 @@ export default function Nav() {
         <div className="nav__hamburgerBtn">
           <Link to='/SignIn' className="nav__logInBtn">Log in</Link>
           <Link to='/SignUp' className="nav__signUpBtn">Sign up</Link>
+					<Link to='/Pages/Mypage' className="nav__myPageBtn">My page</Link>
         </div>
 			  <div className="nav__year">여기에 년도 입력</div>
         <div>
-          <Link to='/Pages/MainPage' className="nav__logoBtn">logo</Link>
+          <Link to='/Pages/MainPage' className="nav__logoBtn">Main</Link>
         </div>
 			</div>
-			<div className="nav__scrollTracer">여기에 원숭이 입력</div>
+			<div className="nav__scrollTracerContainer">
+				<div className="nav__scrollTracer">여기에 원숭이 입력</div>
+			</div>
     </header>
   )
 }
