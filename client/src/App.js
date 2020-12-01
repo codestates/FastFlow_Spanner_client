0e5864584743530eefcc678363a5715f5a5019ff
yumboy8747 <yumboy8747@naver.com>
2020-12-01 15:53:59 +0900

    [Client] / #16 / Add: MainPage

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route, withRouter } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import Footer from './components/Footer';
 import FistAxe from './components/Detail/FistAxe';
+import MainPage from './components/Pages/MainPage';
 
 const App = () => {
   return (
     <div>
       <Switch>
+        <Route exact path="/" render={() => <MainPage />} />
         <Route exact path="/signup" render={() => <SignUp />} />
         <Route exact path="/fistaxe" render={() => <FistAxe />} />
       </Switch>
