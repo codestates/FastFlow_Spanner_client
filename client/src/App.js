530a5f93551145060a32de5120feb5e90c7ac63e
yumboy8747 <yumboy8747@naver.com>
2020-12-01 10:11:02 +0900

    Add: DetailPage, Update: Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import Footer from './components/Footer';
+import FistAxe from './components/Detail/FistAxe';
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/signup" render={() => <SignUp />} />
+        <Route exact path="/fistaxe" render={() => <FistAxe />} />
       </Switch>
       <Footer />
     </div>
